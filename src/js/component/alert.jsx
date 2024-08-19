379d16ff41323ea01c7cbe6975b3b94aad47a140
Sergio Arribas <arribastellezsergio@gmail.com>
2024-08-19 19:07:27 +0000

    feat:alert

---
+++ src/js/component/alert.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const Alert = () => {
+    return (
+        <div class="alert alert-info" role="alert">
+            Vamso progresando!!!!!
+        </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import { Card } from "./Card.jsx";
 import { Landing } from "./Landing.jsx";
 import { Button } from "./Button.jsx";
+import { Alert } from "./alert.jsx";
 
 
 //create your first component
@@ -12,6 +13,7 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<Navbar />
+			<Alert />
 			<Landing />
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
